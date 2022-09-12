a42e36288f709f7bad752abc8f729ae2c90b94e0
Seun Fashina <fashinaoluwaseun36@gmail.com>
2022-09-12 05:58:05 -0700

    created views for checkout, cart and  store page

--- store/views.py
+++ store/views.py
@@ -1,20 +1,59 @@
 from django.shortcuts import render
-
-
-
-
+from .models import Order, OrderItem, Product
 
 def store(request):
-    return render(request, 'store.html')
+    products = Product.objects.all()
+    context={
+        'products': products
+    }
+    return render(request, 'store.html', context)
 
 def cart(request):
-    return render(request, 'cart.html')
+    #this view dynamically renders the item stored in our cart
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        
+        #get the order if exist, created for this currently logged in user
+        #create if it does not exist
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        '''
+        gets all the orderitems whose parent is the order created by this specific customer
+        i.e gets all orderitems that falls under or that is a child of this order
+        A.K.A reverse lookup
+        '''
+
+        items = order.items.all()
+        print(items)
+
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_item_total':0}
+
+    context={
+        'items':items,
+        'order':order
+    }
+    return render(request, 'cart.html', context)
 
 
 
 
 def checkout(request):
-    context = {
 
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+        items = order.items.all()
+
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_item_total':0}
+
+    context={
+        'items':items,
+        'order':order
     }
     return render(request, 'checkout.html', context)
